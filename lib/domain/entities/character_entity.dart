8569ee6ca3d86532449043677005133b6d2a17b9
Daniel Mendoza <danieruone@gmail.com>
2024-03-17 20:15:11 -0500

    :sparkles: Add characters entity

---
+++ lib/domain/entities/character_entity.dart
@@ -0,0 +1,12 @@
+class CharacterEntity {
+  final int id;
+  final String name;
+  final String species;
+  final String image;
+
+  CharacterEntity(
+      {required this.id,
+      required this.name,
+      required this.species,
+      required this.image});
+}
