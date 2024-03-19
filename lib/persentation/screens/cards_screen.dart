5bb1cb77f6f7bbe7c048245e58bbbf8e974409e3
Daniel Mendoza <danieruone@gmail.com>
2024-03-18 21:14:21 -0500

    :sparkles: Add state full widget

--- lib/persentation/screens/cards_screen.dart
+++ lib/persentation/screens/cards_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CardsScreen extends StatelessWidget {
+class CardsScreen extends StatefulWidget {
   const CardsScreen({super.key});
 
+  @override
+  State<CardsScreen> createState() => _CardsScreenState();
+}
+
+class _CardsScreenState extends State<CardsScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
